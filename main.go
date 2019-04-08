66e2b3b5d8ee62cdb2a81316d652dacc0dc3054d
Nathaniel M. Capule <nmcapule@gmail.com>
2019-04-08 11:02:28 +0800

    Add appengine support.

--- main.go
+++ main.go
@@ -19,9 +19,14 @@ func mailingListHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/mailing-success.html")
 }
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
 func main() {
 	fmt.Printf("Running on port %d...", *port)
 
+	http.HandleFunc("/_ah/health", healthCheckHandler)
 	http.HandleFunc("/mailing", mailingListHandler)
 	http.HandleFunc("/", rootHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
